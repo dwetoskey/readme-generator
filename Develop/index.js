6db8e59f34c5ff1b5d13229b559937311b3717a2
dwetoskey <dwetoskey224@gmail.com>
2022-01-31 14:00:24 -0500

    added packages, started question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+
+    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
